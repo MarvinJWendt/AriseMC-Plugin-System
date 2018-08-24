529261e991e510647e33ea2d805e916ac946bea8
Scarface01Dev <pascal.fichtner@web.de>
2018-08-24 16:33:22 +0200

    + Added User

---
+++ src/main/java/de/hardcorepvp/data/User.java
@@ -0,0 +1,22 @@
+package de.hardcorepvp.data;
+
+import java.util.UUID;
+
+public class User {
+
+    private UUID uniqueId;
+    private long firstLogin;
+    private long lastLogin;
+    private long money;
+    private int kills;
+    private int deaths;
+    private int rank;
+
+    public User(UUID uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public UUID getUniqueId() {
+        return uniqueId;
+    }
+}
