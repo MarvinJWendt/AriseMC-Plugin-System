d9a7caf0bb33f61a32a2d19c0978bb805060f77f
Scarface01Dev <pascal.fichtner@web.de>
2018-08-27 19:29:14 +0200

    + Ranking cache

--- src/main/java/de/hardcorepvp/manager/RankingManager.java
+++ src/main/java/de/hardcorepvp/manager/RankingManager.java
@@ -1,14 +1,19 @@
 package de.hardcorepvp.manager;
 
 import de.hardcorepvp.Main;
+import de.hardcorepvp.model.Ranking;
 import net.minecraft.util.com.google.common.cache.CacheBuilder;
 import net.minecraft.util.com.google.common.cache.CacheLoader;
 import net.minecraft.util.com.google.common.cache.LoadingCache;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
@@ -16,6 +21,7 @@ import java.util.function.Consumer;
 public class RankingManager {
 
 	private LoadingCache<UUID, Integer> statsSingleRankCache;
+	private LoadingCache<Ranking, Map<String, Long>> rankingCache;
 
 	public RankingManager() {
 		this.statsSingleRankCache = CacheBuilder
@@ -39,6 +45,72 @@ public class RankingManager {
 						return rank;
 					}
 				});
+		this.rankingCache = CacheBuilder
+				.newBuilder()
+				.expireAfterAccess(5, TimeUnit.MINUTES)
+				.build(new CacheLoader<Ranking, Map<String, Long>>() {
+					@Override
+					public Map<String, Long> load(Ranking ranking) throws Exception {
+						if (ranking == Ranking.KILLS) {
+							PreparedStatement statement = Main.getDatabaseManager().getConnection().prepareStatement("SELECT `uniqueId`, `kills` FROM `user_stats` ORDER BY `kills`");
+							ResultSet resultSet = statement.executeQuery();
+							Map<String, Long> rankingKills = new ConcurrentHashMap<>();
+							while (resultSet.next()) {
+								UUID uniqueId = UUID.fromString(resultSet.getString("uniqueId"));
+								long kills = resultSet.getInt("kills");
+								Player target = Bukkit.getPlayer(uniqueId);
+								if (target.isOnline()) {
+									rankingKills.put(target.getName(), kills);
+								}
+								OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(uniqueId);
+								if (offlinePlayer.hasPlayedBefore()) {
+									rankingKills.put(offlinePlayer.getName(), kills);
+								}
+								rankingKills.put(uniqueId.toString(), kills);
+							}
+							return rankingKills;
+						}
+						if (ranking == Ranking.DEATHS) {
+							PreparedStatement statement = Main.getDatabaseManager().getConnection().prepareStatement("SELECT `uniqueId`, `deaths` FROM `user_stats` ORDER BY `deaths`");
+							ResultSet resultSet = statement.executeQuery();
+							Map<String, Long> rankingDeaths = new ConcurrentHashMap<>();
+							while (resultSet.next()) {
+								UUID uniqueId = UUID.fromString(resultSet.getString("uniqueId"));
+								long deaths = resultSet.getInt("deaths");
+								Player target = Bukkit.getPlayer(uniqueId);
+								if (target.isOnline()) {
+									rankingDeaths.put(target.getName(), deaths);
+								}
+								OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(uniqueId);
+								if (offlinePlayer.hasPlayedBefore()) {
+									rankingDeaths.put(offlinePlayer.getName(), deaths);
+								}
+								rankingDeaths.put(uniqueId.toString(), deaths);
+							}
+							return rankingDeaths;
+						}
+						if (ranking == Ranking.MONEY) {
+							PreparedStatement statement = Main.getDatabaseManager().getConnection().prepareStatement("SELECT `uniqueId`, `money` FROM `user_money` ORDER BY `money`");
+							ResultSet resultSet = statement.executeQuery();
+							Map<String, Long> rankingMoney = new ConcurrentHashMap<>();
+							while (resultSet.next()) {
+								UUID uniqueId = UUID.fromString(resultSet.getString("uniqueId"));
+								long money = resultSet.getLong("money");
+								Player target = Bukkit.getPlayer(uniqueId);
+								if (target.isOnline()) {
+									rankingMoney.put(target.getName(), money);
+								}
+								OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(uniqueId);
+								if (offlinePlayer.hasPlayedBefore()) {
+									rankingMoney.put(offlinePlayer.getName(), money);
+								}
+								rankingMoney.put(uniqueId.toString(), money);
+							}
+							return rankingMoney;
+						}
+						return null;
+					}
+				});
 	}
 
 	public void getUserStatsRank(UUID uniqueId, Consumer<Integer> consumer) {
@@ -60,5 +132,25 @@ public class RankingManager {
 			e.printStackTrace();
 			return -1;
 		}
+	}
+
+	public void getRanking(Ranking ranking, Consumer<Map<String, Long>> consumer) {
+		Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> {
+			try {
+				consumer.accept(this.rankingCache.get(ranking));
+			} catch (Exception e) {
+				e.printStackTrace();
+				consumer.accept(null);
+			}
+		});
+	}
+
+	public Map<String, Long> getRankingSync(Ranking ranking) {
+		try {
+			return this.rankingCache.get(ranking);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
---
+++ src/main/java/de/hardcorepvp/model/Ranking.java
@@ -0,0 +1,5 @@
+package de.hardcorepvp.model;
+
+public enum Ranking {
+	KILLS, DEATHS, MONEY
+}
