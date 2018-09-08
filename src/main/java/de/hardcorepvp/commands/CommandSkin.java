edd2f0ea8c66a9a0998d9f6af1bdc71e79eb32a4
Marvin Kokor <kokor@gmx.de>
2018-09-09 00:32:35 +0200

    added trycatch to /skin

--- src/main/java/de/hardcorepvp/commands/CommandSkin.java
+++ src/main/java/de/hardcorepvp/commands/CommandSkin.java
@@ -20,58 +20,55 @@ public class CommandSkin implements CommandExecutor {
 		if (!(sender instanceof Player)) {
 			return false;
 		}
-
 		Player player = (Player) sender;
 
 		if (args.length == 0) {
-			String[] props = Utils.getSkinData(player.getName());
-			GameProfile gp = ((CraftPlayer) player).getProfile();
-			gp.getProperties().clear();
-			gp.getProperties().put("textures", new Property("textures", props[0], props[1]));
-
-			for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-				onlinePlayer.hidePlayer(player);
-				onlinePlayer.showPlayer(player);
-			}
-			
-			return true;
-
-		}
-
-		if (args.length == 1) {
-			if (Bukkit.getPlayer(args[0]) != null) {
-				Property props = Utils.getSkinData(Bukkit.getPlayer(args[0]));
-				GameProfile gp = ((CraftPlayer) player).getProfile();
-				gp.getProperties().clear();
-				gp.getProperties().put("textures", new Property("textures", props.getValue(), props.getSignature()));
-
-				for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-					onlinePlayer.hidePlayer(player);
-					onlinePlayer.showPlayer(player);
-				}
-
-			} else {
-
-				String[] props = Utils.getSkinData(args[0]);
+			try {
+				String[] props = Utils.getSkinData(player.getName());
 				GameProfile gp = ((CraftPlayer) player).getProfile();
 				gp.getProperties().clear();
 				gp.getProperties().put("textures", new Property("textures", props[0], props[1]));
-
+				player.sendMessage("Du hast deinen Skin resetet");
 				for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
 					onlinePlayer.hidePlayer(player);
 					onlinePlayer.showPlayer(player);
 				}
-
+			} catch (Exception exception) {
+				player.sendMessage("Dein Skin konnte aufgrund von zu vielen Requests an die Mojang API nicht geladen werden!");
 			}
 			return true;
-
 		}
-
+		if (args.length == 1) {
+			try {
+				if (Bukkit.getPlayer(args[0]) != null) {
+					Property props = Utils.getSkinData(Bukkit.getPlayer(args[0]));
+					GameProfile gp = ((CraftPlayer) player).getProfile();
+					gp.getProperties().clear();
+					gp.getProperties().put("textures", new Property("textures", props.getValue(), props.getSignature()));
+					player.sendMessage("Du hast nun einen neuen Skin");
+					for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+						onlinePlayer.hidePlayer(player);
+						onlinePlayer.showPlayer(player);
+					}
+				} else {
+					String[] props = Utils.getSkinData(args[0]);
+					GameProfile gp = ((CraftPlayer) player).getProfile();
+					gp.getProperties().clear();
+					gp.getProperties().put("textures", new Property("textures", props[0], props[1]));
+					player.sendMessage("Du hast nun einen neuen Skin");
+					for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+						onlinePlayer.hidePlayer(player);
+						onlinePlayer.showPlayer(player);
+					}
+				}
+			} catch (Exception exception) {
+				player.sendMessage("Dein Skin konnte aufgrund von zu vielen Requests an die Mojang API nicht geladen werden!");
+			}
+			return true;
+		}
 		player.sendMessage(Messages.formatMessage(Messages.TOO_MANY_ARGUMENTS));
 		return true;
-
 	}
-
 }
 
 
